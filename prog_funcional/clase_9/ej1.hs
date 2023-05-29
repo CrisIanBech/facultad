e9bd27f558216d5313ab2012568e2cd938be797f
CrisIanBech <cristopherbechtholdt@gmail.com>
2023-05-29 03:06:16 -0300

    Mitad ej1.b

--- prog_funcional/clase_9/ej1.hs
+++ prog_funcional/clase_9/ej1.hs
@@ -91,8 +91,111 @@ i.
     = def. id
     Suma ex1 ex2
 
-    
-    
+    Dem. caso inductivo 2:
 
+    LI:
+    ea2ExpA . exp2A2ea (Prod ex1 ex2)
+    = def. op. .
+    ea2ExpA (exp2A2ea (Prod ex1 ex2))
+    = def. exp2A2ea.3
+    ea2ExpA (Bop Prod (expA2ea ex1) (expA2ea ex2))
+    = def. ea2ExpA.3
+    BOp binOp2ExpA Mul ea2ExpA (expA2ea ex1) ea2ExpA (expA2ea ex2)
+    = def. op. .
+    BOp binOp2ExpA Mul (ea2ExpA . expA2ea ex1) (ea2ExpA . expA2ea ex2)
+    = HI.1 y HI.2
+    BOp binOp2ExpA Mul (id ex1) (id ex2)
+    = def. id
+    BOp binOp2ExpA Mul ex1 ex2
+    = def. binOp2ExpA.2
+    Prod ex1 ex2
+
+    LD:
+    id (Prod ex1 ex2)
+    = def. id
+    Prod ex1 ex2
+    
+    Se cumple para los 3 casos. Por lo tanto, la propiedad es válida.
+
+    ii.
+    Prop.: ¿expA2ea . ea2ExpA = id?
+    Dem.: Por ppio. de extensionalidad, es equivalente demostrar que:
+    ¿Para todo x :: ea. expA2ea . ea2ExpA X = id X?
+    Sea n :: EA. Por ppio de inducción en la estructura de n,
+    es equivalente demostrar que
+    ¿expA2ea . ea2ExpA n = id n?
+
+    Caso base: n = Const n )
+        ¿expA2ea . ea2ExpA (Const n) = id (Const n)?
+
+    Caso inductivo: n = (BOp bop e1 e2)
+        HI.1) ¡expA2ea . ea2ExpA e1 = id e1!
+        HI.2) ¡expA2ea . ea2ExpA e2 = id e2!
+        TI) ¿expA2ea . ea2ExpA (BOp bop e1 e2) = id (BOp bop e1 e2)?
+
+    Dem. caso base:
+        LI:
+        expA2ea . ea2ExpA (Const n)
+        = def. op. .
+        expA2ea (ea2ExpA (Const n))
+        = def. ea2ExpA.1
+        expA2ea (Cte n)
+        = def. expA2ea
+        Const n
+
+        LD:
+        id (Const n)
+        = def. id
+        Const n
+
+    Dem. caso inductivo:
+        LI:
+        expA2ea . ea2ExpA (BOp bop e1 e2)
+        = def. op. .
+        expA2ea (ea2ExpA (BOp bop e1 e2))
+        = def. ea2ExpA
+        expA2ea (binOp2ExpA bop ea2ExpA exp1 ea2ExpA exp2)
+        = def. binOp2ExpA
+        Por análisis de casos:
+            bop = Sum
+            expA2ea (binOp2ExpA Sum ea2ExpA exp1 ea2ExpA exp2)
+            = def. binOp2ExpA.1
+            exp2A2ea (Suma (ea2ExpA exp1) (ea2ExpA exp2))
+            = def. exp2A2ea.2
+            BOp Sum (expA2ea (ea2ExpA exp1)) (expA2ea (ea2ExpA exp2))
+            = def. op. .
+            BOp Sum (expA2ea . ea2ExpA exp1) (expA2ea . ea2ExpA exp2)
+            = HI.1 y HI.2
+            BOp Sum (id exp1) (id exp2)
+            = def. id
+            BOp Sum exp1 exp2
+
+            bop = Mul
+            expA2ea (binOp2ExpA Mul ea2ExpA exp1 ea2ExpA exp2)
+            = def. binOp2ExpA.2
+            exp2A2ea (Prod (ea2ExpA exp1) (ea2ExpA exp2))
+            = def. exp2A2ea.3
+            BOp Mul (expA2ea (ea2ExpA exp1)) (expA2ea (ea2ExpA exp2))
+            = def. op. .
+            BOp Mul (expA2ea . ea2ExpA exp1) (expA2ea . ea2ExpA exp2)
+            = HI.1 y HI.2
+            BOp Mul (id exp1) (id exp2)
+            = def. id
+            BOp Mul exp1 exp2
+
+        LD:
+        Por análisis de casos:
+            bop = Mul
+            id (BOp Mul e1 e2)
+            = def. id
+            BOp Mul e1 e2
+
+            bop = Sum
+            id (BOp Sum e1 e2)
+            = def. id
+            BOp Sum e1 e2
+
+            Se llega al mismo resultado para ambos casos. Se cumplen todos los
+            casos. Por ende, es válida la propiedad.
 
     
